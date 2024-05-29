ddf983f918bce537f5eff002c62153447cb80107
StarHeartHunt <starheart233@gmail.com>
2024-05-29 22:07:05 +0800

    refactor: cleanup

--- tuan_batch_study/__main__.py
+++ tuan_batch_study/__main__.py
@@ -1,31 +1,33 @@
-from loguru import logger
-
-from .constants import logger_config
+from .log import logger
 from .utils import get_sign, get_token, save_history, get_chapter_id
 
-logger.configure(**logger_config)
-
 if __name__ == "__main__":
-    for line in open("mid.txt", "r", encoding="utf-8"):
+    for line in open("mid.txt", encoding="utf-8"):
         mid = line.strip()
         # 跳过空行
         if mid.isspace():
             continue
         logger.info(f"开始执行{mid}的学习记录上报")
+
         try:
             cid = get_chapter_id()
-            # logger.debug(cid)
+            logger.debug(cid)
+
             sign = get_sign(mid)
-            # logger.debug(mid, sign)
+            logger.debug(mid, sign)
+
             token = get_token(sign)
-            # logger.debug(token)
+            logger.debug(token)
+
             res = save_history(token, cid)
-            # logger.debug(res)
+            logger.debug(res)
+
         except Exception:
             logger.opt(exception=True).info("请求时发生错误")
             continue
 
         if res["errno"] == 0:
             logger.info("保存观看记录成功")
+
         else:
             logger.error(f"响应发生错误({res['errno']}): {res['errmsg']}")
--- tuan_batch_study/constants.py
+++ tuan_batch_study/constants.py
@@ -1,11 +1,9 @@
-import sys
-
 api_headers = {
     "Host": "tuanapi.12355.net",
     "Connection": "keep-alive",
     "Accept": "application/json, text/javascript, */*; q=0.01",
     "Origin": "https://tuan.12355.net",
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; MIX 3 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045713 Mobile Safari/537.36 MMWEBID/9711 MicroMessenger/8.0.7.1920(0x28000753) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; MIX 3 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045713 Mobile Safari/537.36 MMWEBID/9711 MicroMessenger/8.0.7.1920(0x28000753) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",  # noqa: E501
     "X-Requested-With": "com.tencent.mm",
     "Sec-Fetch-Site": "same-site",
     "Sec-Fetch-Mode": "cors",
@@ -20,7 +18,7 @@ youthstudy_headers = {
     "Origin": "https://youthstudy.12355.net",
     "X-Litemall-Token": "",
     "X-Litemall-IdentiFication": "young",
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; MIX 3 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045713 Mobile Safari/537.36 MMWEBID/9711 MicroMessenger/8.0.7.1920(0x28000753) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; MIX 3 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045713 Mobile Safari/537.36 MMWEBID/9711 MicroMessenger/8.0.7.1920(0x28000753) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",  # noqa: E501
     "Accept": "*/*",
     "X-Requested-With": "com.tencent.mm",
     "Sec-Fetch-Site": "same-origin",
@@ -36,18 +34,3 @@ get_chapter_url = "https://youthstudy.12355.net/apih5/api/young/chapter/new"
 save_history_url = (
     "https://youthstudy.12355.net/apih5/api/young/course/chapter/saveHistory"
 )
-
-logger_config = {
-    "handlers": [
-        {
-            "sink": sys.stdout,
-            "format": (
-                "<g>{time:MM-DD HH:mm:ss}</g> "
-                "[<lvl>{level}</lvl>] "
-                "<c><u>{name}</u></c> | "
-                # "<c>{function}:{line}</c>| "
-                "{message}"
-            ),
-        }
-    ]
-}
