6e23045cd1cda26c12c4ef681564e75c5cdc4cd6
StarHeartHunt <starheart233@gmail.com>
2024-05-29 22:01:17 +0800

    refactor: move log logic to file

---
+++ tuan_batch_study/log.py
@@ -0,0 +1,53 @@
+import sys
+import inspect
+import logging
+from typing import TYPE_CHECKING
+
+import loguru
+
+if TYPE_CHECKING:
+    from loguru import Logger, Record
+
+logger: "Logger" = loguru.logger
+
+
+# https://loguru.readthedocs.io/en/stable/overview.html#entirely-compatible-with-standard-logging
+class LoguruHandler(logging.Handler):
+
+    def emit(self, record: logging.LogRecord):
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+def default_filter(record: "Record"):
+    log_level = "INFO"
+    levelno = logger.level(log_level).no if isinstance(log_level, str) else log_level
+    return record["level"].no >= levelno
+
+
+default_format: str = (
+    "<g>{time:MM-DD HH:mm:ss}</g> "
+    "[<lvl>{level}</lvl>] "
+    "<c><u>{name}</u></c> | "
+    "{message}"
+)
+
+logger.remove()
+logger_id = logger.add(
+    sys.stdout,
+    level=0,
+    diagnose=False,
+    filter=default_filter,
+    format=default_format,
+)
